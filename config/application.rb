3f1236a1bc874f97a60e58e8a8877493ba0b1fb9
judyhe <judy.w.he@gmail.com>
2012-03-01 14:37:16 -0500

    assets precompile config

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,8 @@ module Tic
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.assets.initialize_on_precompile = false # don't access the DB or load models when precompiling your assets.
+    
   end
 end
